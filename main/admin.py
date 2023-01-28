63db65c7999b39a21f126df62a90cc5d70c3969b
Hayk Arshakyan <haykarshakyan@hotmail.com>
2023-01-28 15:19:48 +0400

    commit

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django.template.defaultfilters import truncatechars
+
 from .models import *
 
+class HTMLtags(admin.ModelAdmin):
+    list_display = ('tagname', 'descriptionArm', 'descriptionRus', 'syntax', 'example')
+    fieldsets = [('Tag', {'fields': ['tagname']}), ('Description',{'fields': ['descriptionArm', 'descriptionRus']}), ('Syntax', {'fields': ['syntax']}), ('Example', {'fields': ['example']})]
+
+    def descriptionRus(self, obj):
+        return obj.descriptionRus[:100]
+
 admin.site.register(Lessons)
-admin.site.register(HTMLTags)
+admin.site.register(HTMLTags, HTMLtags)
